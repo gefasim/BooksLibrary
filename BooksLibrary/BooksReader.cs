2d8e8913566afc7c832cfe6035a05057febcbd8b
Oleksandr Oliynyk <vov96now@gmail.com>
2025-11-21 12:58:19 +0200

    Update summary

--- BooksLibrary/BooksReader.cs
+++ BooksLibrary/BooksReader.cs
@@ -6,7 +6,7 @@ namespace BooksLibrary;
 public class BooksReader
 {
     /// <summary>
-    /// Reads XML file with Books.
+    /// Reads an XML file with Books.
     /// Creates a new file on a step <see cref="BookService.Save()"/> if file doesn't exists.
     /// </summary>
     /// <param name="path">books xml file path</param>
@@ -14,16 +14,13 @@ public class BooksReader
     public static BooksService ReadOrCreateFile(string path)
     {
         if (!File.Exists(path))
-        {
             return new BooksService(new BooksWrapper(), path);
-        }
 
         try
         {
             var serializer = new XmlSerializer(typeof(BooksWrapper));
-            BooksWrapper? books;
             using var stream = new FileStream(path, FileMode.Open);
-            books = serializer.Deserialize(stream) as BooksWrapper;
+            BooksWrapper? books = serializer.Deserialize(stream) as BooksWrapper;
             return new BooksService(books ?? new BooksWrapper(), path);
         } catch (InvalidOperationException e)
         {
